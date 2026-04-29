91039c20cfa5c0090ed9ebbcc9e3d24efa1e44c6
Luyer <luyer69@gmail.com>
2026-04-29 08:47:41 +0200

    Create GetColors.lua
    First creation

---
+++ GetColors.lua
@@ -0,0 +1,7 @@
+-- ============================================================
+-- PLUGIN BLOCK COLOR IN SCHEMATIC (required)
+-- Returns { R, G, B } or { R, G, B, Alpha } -- range 0-255
+-- ============================================================
+function GetColor(props)
+  return Colors.Plugin
+end
